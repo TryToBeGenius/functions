8d35a0ce95c1b93484df754fa7be2d931d390ad7
Vladimir <JSdev.kuznetsov@yandex.ru>
2024-05-23 19:51:37 +0500

    Задача 3 решена молодес!)

---
+++ homework3/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] array = { 1, 2, 3, 4, 5};
+
+
+void PrintArray()
+{
+    
+    int tempArr = 0;
+    
+    for (int i = 0; i < array.Length/2; i++)
+    {   tempArr = array[i];
+        array[i] = array[array.Length - i - 1];
+        array[array.Length - i - 1] = tempArr;
+           
+        
+    }
+    Console.Write("[");
+    for(int j = 0; j < array.Length-1; j++)
+        {
+            Console.Write(array[j] + ",");
+        }
+    Console.Write(array[array.Length-1]);
+    Console.Write("]");
+}
+
+PrintArray();
+
